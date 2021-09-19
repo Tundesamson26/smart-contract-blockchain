ae2acfc9f55679f0016ab51fc267809a8d5c4696
Tunde Samson <odewoletundesamson@gmail.com>
2021-09-19 18:44:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+// Source code to interact with smart contract
+
+// web3 provider with fallback for old version
+if (window.ethereum) {
+  window.web3 = new Web3(window.ethereum)
+  try {
+      // ask user for permission
+      ethereum.enable()
+      // user approved permission
+  } catch (error) {
+      // user rejected permission
+      console.log('user rejected permission')
+  }
+}
+else if (window.web3) {
+  window.web3 = new Web3(window.web3.currentProvider)
+  // no need to ask for permission
+}
+else {
+  window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+}
+console.log (window.web3.currentProvider)
+
+// contractAddress and abi are setted after contract deploy
+var contractAddress = '0x738AA755E6A4e609aF99a62DD4943Ba3096Abe5C';
+var abi = JSON.parse( '[{"constant":true,"inputs":[],"name":"getInfo","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_info","type":"string"}],"name":"setInfo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]' );
+
+//contract instance
+contract = new web3.eth.Contract(abi, contractAddress);
+
+// Accounts
+var account;
+
+web3.eth.getAccounts(function(err, accounts) {
+  if (err != null) {
+    alert("Error retrieving accounts.");
+    return;
+  }
+  if (accounts.length == 0) {
+    alert("No account found! Make sure the Ethereum client is configured properly.");
+    return;
+  }
+  account = accounts[0];
+  console.log('Account: ' + account);
+  web3.eth.defaultAccount = account;
+});
+
+//Smart contract functions
+function registerSetInfo() {
+  info = $("#newInfo").val();
+  contract.methods.setInfo (info).send( {from: account}).then( function(tx) { 
+    console.log("Transaction: ", tx); 
+  });
+  $("#newInfo").val('');
+}
+
+function registerGetInfo() {
+  contract.methods.getInfo().call().then( function( info ) { 
+    console.log("info: ", info);
+    document.getElementById('lastInfo').innerHTML = info;
+  });    
+}
+
+// web3 provider with fallback for old version
+window.addEventListener('load', async () => {
+  // New web3 provider
+  if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      try {
+          // ask user for permission
+          await ethereum.enable();
+          // user approved permission
+      } catch (error) {
+          // user rejected permission
+          console.log('user rejected permission');
+      }
+  }
+  // Old web3 provider
+  else if (window.web3) {
+      window.web3 = new Web3(web3.currentProvider);
+      // no need to ask for permission
+  }
+  // No web3 provider
+  else {
+      console.log('No web3 provider detected');
+  }
+});
